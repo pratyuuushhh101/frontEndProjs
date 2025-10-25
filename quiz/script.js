3353374ad8273f49ded2bd0118e4da248c118e24
pratz <pratyushagrawal2409@gmail.com>
2025-10-25 14:26:14 +0530

    quiz updated

--- quiz/script.js
+++ quiz/script.js
@@ -1,4 +1,4 @@
-// DOM Elements
+
 const startScreen = document.getElementById("start-screen");
 const quizScreen = document.getElementById("quiz-screen");
 const resultScreen = document.getElementById("result-screen");
