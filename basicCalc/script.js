4baf4289b702d1e4c451985e60cba8b2b9af3cd6
pratz <pratyushagrawal2409@gmail.com>
2025-10-25 13:45:28 +0530

    basicCalcPorj

---
+++ basicCalc/script.js
@@ -0,0 +1,32 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    let result = eval(display.innerText);
+                    if (isNaN(result) || result == Infinity) {
+                        display.innerText = 'undefined';
+                    } else {
+                        display.innerText = result;
+                    }
+                }catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
